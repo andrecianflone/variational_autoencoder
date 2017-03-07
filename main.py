d37702c164b5b51652e5d3596c8d3b15a1b2c90c
Andre <apcianflone@gmail.com>
2017-03-07 18:09:02 -0500

    updated plotter

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 Writer = animation.writers['ffmpeg']
-writer = Writer(fps=1, metadata=dict(artist='Me'), bitrate=1800)
+writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800)
 from scipy.stats import norm
 
 # Get data
@@ -21,14 +21,15 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
 size_train = mnist.train.num_examples # should be 55k
 
-# Hyperparams
+# Params
 batch_size = 100
+nb_epoch = 5
 
 class VAE():
   def __init__(self):
     self.input_dim = 784
     self.h_dim = 256 # hidden layer dimension
-    self.latent_dim = 3 # size of latent state hidden layer
+    self.latent_dim = 2 # size of latent state hidden layer
     self.data_type = tf.float32
 
     # The MNIST inputs
@@ -117,17 +118,24 @@ class Train():
   def __init__(self):
     # Declare our VAE model
     self.model = VAE()
+    num_train_batches = mnist.train.num_examples // batch_size
+    self.last_batch_loss = None # loss on most recent trained batch
 
-    fig = plt.figure(figsize=(6, 6))
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_xlim(-8, 8)
-    ax.set_xlim(-10, 10)
-    ax.set_zlim(-10, 10)
-    # ax = plt.axes(xlim=(-8, 8), ylim=(-10, 10), zlim=(-10, 10))
+    self.fig = plt.figure(figsize=(6, 6))
+    self.ax = self.fig.add_subplot(111)
+    # self.ax = self.fig.add_subplot(111, projection='3d')
+    ax = plt.axes(xlim=(-12, 12), ylim=(-12, 12))
     self.epoch_time = datetime.now()
-    self.data_stream = self.inc_train_yield_test_pred()
     self.cur_epoch_train = 0
-    nb_epoch = 5
+    batch_callback = True
+    self.data_stream = self.incr_train_yield_test_pred(batch_callback=batch_callback)
+    plot_n_frames_per_epoch = 30 # number of frames to visualize per epoch
+    self.plot_every_nth_frame = num_train_batches // plot_n_frames_per_epoch
+
+    if batch_callback:
+      frames = nb_epoch*self.num_train_batches-2
+    else:
+      frames = nb_epoch - 2
 
     # Context manager for convenience
     # Uses default graph
@@ -140,13 +148,11 @@ class Train():
       print('*'*79)
 
       # Training and testing is managed by matplotlib iterator
-      # Decrement nb_epoch in frames since init_plot includes one iter
       animator = animation.FuncAnimation(
-              fig, self.update_plot, init_func=self.init_plot,
-              interval=100, frames=nb_epoch-1, blit=True, repeat=False)
+              self.fig, self.update_plot, init_func=self.init_plot,
+              interval=100, frames=frames, blit=True, repeat=False)
       # animator.save('animation.mp4', writer=writer)
       # animator.save('animation.mpeg', writer=writer)
-      # animator.save('animation.mpeg', writer=writer, extra_args=['-vcodec', 'libx264'])
       plt.show()
 
   def init_plot(self):
@@ -155,7 +161,12 @@ class Train():
       Must return the scatter object so animate knows what to update
     """
     pred, labels = next(self.data_stream)
-    self.scat = plt.scatter(pred[:,0], pred[:,1], pred[:,2], c=labels)
+    maxs = np.max(pred, axis=0)
+    mins = np.min(pred, axis=0)
+    # self.ax.set_xlim(mins[0], maxs[0])
+    # self.ax.set_xlim(mins[1], maxs[1])
+    # self.ax.set_zlim(mins[2], maxs[2])
+    self.scat = plt.scatter(pred[:,0], pred[:,1], c=labels)
     plt.colorbar()
     return [self.scat]
 
@@ -168,10 +179,9 @@ class Train():
 
   def get_encoded_test(self):
     """ Returns array of [data_size, latent_size] and labels [data_size,]"""
-    num_test_batches = mnist.test.num_examples // batch_size
     pred = np.zeros((mnist.test.num_examples, self.model.latent_dim))
     labels = np.zeros((mnist.test.num_examples))
-    for i in range(num_test_batches):
+    for i in range(self.num_test_batches):
       shift = i * batch_size
       batch_xs, batch_ys = mnist.train.next_batch(batch_size)
       fetch = [self.model.z_mean]
@@ -181,33 +191,45 @@ class Train():
       labels[shift:shift+batch_size] = batch_ys
     return pred, labels
 
-  def inc_train_yield_test_pred(self):
-    """ For each yield, increments training by one epoch,
-    returns test data predictions"""
-    while True: # the animator decides when to stop
-      self.train_one_epoch()
+  def incr_train_yield_test_pred(self, batch_callback=False):
+    """ Yields predictions and labels of test set after an epoch or batch """
+    while True: # the animator decides when to stop the generator
+      if batch_callback: # yield after one batch
+        self.train_one_batch()
+        if mnist.train.epochs_completed > self.cur_epoch_train:
+          self.process_end_epoch()
+      else: # train a whole epoch before yielding
+        self.train_one_epoch()
       pred, labels = self.get_encoded_test()
       yield pred, labels
+
+  def train_one_batch(self):
+    """ Train only one batch, so can visualize results per batch """
+    batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+    fetch = [self.model.optimizer, self.model.recon_loss, self.model.kl_loss, self.model.loss]
+    feed={self.model.x: batch_xs}
+    _, recon_loss, kl_loss, loss = self.sess.run(fetch, feed)
+    self.last_batch_loss = loss
+    t2 = datetime.now()
+    diff_t = (t2 - self.epoch_time).total_seconds()
+    print('epoch: {:2.0f} time: {:2.1f} | loss: {:.4f}'.format(
+        self.cur_epoch_train, diff_t, loss), end='\r')
 
   def train_one_epoch(self):
     """ Trains model for a single epoch """
     while mnist.train.epochs_completed == self.cur_epoch_train:
-      batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-      fetch = [self.model.optimizer, self.model.recon_loss, self.model.kl_loss, self.model.loss]
-      feed={self.model.x: batch_xs}
-      _, recon_loss, kl_loss, loss = self.sess.run(fetch, feed)
-      t2 = datetime.now()
-      diff_t = (t2 - self.epoch_time).total_seconds()
-      print('epoch: {:2.0f} time: {:2.1f} | loss: {:.4f}'.format(
-          self.cur_epoch_train, diff_t, loss), end='\r')
+      self.train_one_batch()
+    self.process_end_epoch()
 
-    self.cur_epoch_train = mnist.train.epochs_completed # increment class epoch
+  def process_end_epoch(self):
+    """ Print some info at the end of an epoch """
+    self.cur_epoch_train = mnist.train.epochs_completed
     # At end of epoch, print new line
     t2 = datetime.now()
     diff_t = (t2 - self.epoch_time).total_seconds()
     epochs_completed = mnist.train.epochs_completed
     print('epoch: {:2.0f} time: {:2.1f} | loss: {:.4f}'.format(
-      self.cur_epoch_train, diff_t, loss), end="\n")
+      self.cur_epoch_train, diff_t, self.last_batch_loss), end="\n")
     self.epoch_time = datetime.now()
 
 train = Train()
